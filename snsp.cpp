da9ec363eea2a6eda9be737c878dbe1d1fe90066
Uriy Dolewsky <dol3wsky@gmail.com>
2026-04-03 23:25:24 +0300

    add array for cout

--- snsp.cpp
+++ snsp.cpp
@@ -1,5 +1,5 @@
-#include <iostream>          // для вывода в консоль
-#include <string>            // для работы со строками
+#include <iostream>          
+#include <string>            
 #include <cstring> 
 //THREAD 
 #include <condition_variable>
@@ -17,6 +17,8 @@
 #include <fcntl.h>
 #include <cerrno>
 #include <poll.h>
+
+#define MAX_PORT 1024
 
 using namespace std;
 
@@ -41,6 +43,15 @@ class ThreadPool{
                             tasks_.pop();
                         }
                         task();
+                        // добавляем wait и cчетчик активных задач
+                        {
+                            unique_lock<mutex> lock(queue_mutex_);
+                            active_tasks--;
+                            if (tasks_.empty() && active_tasks == 0) {
+                                done_cv_.notify_one();
+                            }
+                        }
+
                     }
                 });
                     
@@ -64,15 +75,25 @@ class ThreadPool{
             {
                 unique_lock<mutex> lock(queue_mutex_);
                 tasks_.emplace(move(task));
+                active_tasks++;
             }
             cv_.notify_one();
+        }
+
+         void wait() {
+            unique_lock<mutex> lock(queue_mutex_);
+            done_cv_.wait(lock, [this] {
+                return tasks_.empty() && active_tasks == 0;
+            });
         }
     private:
         vector<thread> threads_;
         queue<function<void()>> tasks_;
         mutex queue_mutex_;
         condition_variable cv_;
+        condition_variable done_cv_;
         bool stop_ = false;
+        int active_tasks;
 
 };
 
@@ -92,6 +113,7 @@ int net_dial(const string& ip, int port)
 
     if (inet_pton(AF_INET,ip.c_str(), &addr.sin_addr) < 1) {
         cerr << "not support format:" << strerror(errno) << endl;
+        close(sockfd);
         return(-1);
     }    
     
@@ -134,16 +156,18 @@ int net_dial(const string& ip, int port)
             
         }
     }
-}
-// Мьютекс для синхронизации вывода
-mutex cout_mutex;
 
-void check(const std::string& ip, int port){
+    close(sockfd);
+    return -1;  // ВАЖНО: возврат при любой неучтённой ситуации
+}
+
+void check(const std::string& ip, int port, bool* arr_ports){
     int conn = net_dial(ip,port);
     if (conn > -1){
-        lock_guard<mutex> lock(cout_mutex);
-        std::cout << "Port " << port << " Is open\n";
+        // lock_guard<mutex> lock(cout_mutex);
+        arr_ports[port] = true;
         close(conn);
+
     }
 }
 
@@ -151,21 +175,37 @@ int main(int argc, char** argv){
 
     if (argc == 2){
         const string scanme = argv[1];
+
+        bool open_ports[MAX_PORT+1] = {false};
+
         // scanme.nmap.org = 45.33.32.156
-        ThreadPool pool(10);
-        for (int i = 1; i<=1024;i++){
-            pool.enqueue([scanme,i]{
-                    check(scanme,i);
+        ThreadPool pool(100);
+        for (int i = 1; i < MAX_PORT+1;i++){
+            pool.enqueue([&open_ports,scanme,i]{
+                    check(scanme,i,open_ports);
                 }
             );
         }
+        //TODO: использовать массив вместо мьютексов
         //TODO: реализовать список самых часто используемых портов 
         //TODO: лучше сделать функцию wait()
         this_thread::sleep_for(chrono::seconds(100));
+
+        // pool.wait();
+
+        //вывод
+        for (int port = 1;port <= MAX_PORT;port++){
+            if (open_ports[port]){
+                std::cout << "Port " << port << " Is open\n";
+            }
+        }
+
+        return 0;
+
     }   
     else{
          cerr << "Usage: snsp 127.0.0.1" << endl;
-
+         return -1;
     }  
-    return 0;
+    
 }
